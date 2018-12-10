d74d66ab6c5e03872c326115bc159be2e766e9bd
Kendragon <Kendragon@PHRONTISTERY>
2018-12-10 12:26:46 -0800

    add tables to context

--- Ling/Data/LingDbContext.cs
+++ Ling/Data/LingDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ling.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,8 @@ namespace Ling.Data
         {
 
         }
+
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Recording> Recordings { get; set; }
     }
 }
