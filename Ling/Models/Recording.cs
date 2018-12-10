76b592641f4f6c4876529dfe0c80ed9e0919c505
Rebecca Hong <rebeccayhong@gmail.com>
2018-12-10 12:48:08 -0800

    Add Recording model

---
+++ Ling/Models/Recording.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Ling.Models
+{
+    public class Recording
+    {
+        public int ID { get; set; }
+        public int LanguageID { get; set; }
+        public byte Audio { get; set; }
+        public List<string> AlternateLanguages { get; set; }
+        public decimal Length { get; set; }
+
+        // Navigation prop
+        public ICollection<Language> Languages { get; set; }
+    }
+}
