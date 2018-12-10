fad80994c1c182d27096e62daf55475fafc72f52
Rebecca Hong <rebeccayhong@gmail.com>
2018-12-10 13:49:37 -0800

    Add db context prop and interface members in Recording service

--- Ling/Models/Services/RecordingService.cs
+++ Ling/Models/Services/RecordingService.cs
@@ -1,8 +1,37 @@
-﻿using Ling.Models.Interfaces;
+﻿using Ling.Data;
+using Ling.Models.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Ling.Models.Services
 {
     public class RecordingService : IRecording
     {
+        public LingDbContext _context { get; set; }
+
+        public Task AddRecording(Recording recording)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteRecording(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Recording> GetRecording(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recording>> GetRecordings()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateRecording(Recording recording)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
