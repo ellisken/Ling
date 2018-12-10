40c75a892191206d244faeceb2751a29b9cd8918
Kendragon <Kendragon@PHRONTISTERY>
2018-12-10 12:24:04 -0800

    add basic language model properties

--- Ling/Models/Language.cs
+++ Ling/Models/Language.cs
@@ -7,5 +7,12 @@ namespace Ling.Models
 {
     public class Language
     {
+        public int ID { get; set; }
+        public string EnglishName { get; set; }
+        public string OriginalName { get; set; }
+        public string Romanized { get; set; }
+        public string ISOCode { get; set; }
+
+        public ICollection<Recording> Recordings { get; set; }
     }
 }
