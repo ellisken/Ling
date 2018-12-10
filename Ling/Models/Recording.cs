3cc62f0e19d56f042aece876dcffc472e2852919
Kendragon <Kendragon@PHRONTISTERY>
2018-12-10 12:58:20 -0800

    change language nav prop of recording from list to single

--- Ling/Models/Recording.cs
+++ Ling/Models/Recording.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ling.Models
 {
     public class Recording
     {
         public int ID { get; set; }
+        [ForeignKey(name: Language.ID)]
         public int LanguageID { get; set; }
         public byte Audio { get; set; }
         public List<string> AlternateLanguages { get; set; }
         public decimal Length { get; set; }
 
         // Navigation prop
-        public ICollection<Language> Languages { get; set; }
+        public Language Language { get; set; }
     }
 }
