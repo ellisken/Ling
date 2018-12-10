757d7d0fa3344ef33d38f142faa9ce522de603de
Kendragon <Kendragon@PHRONTISTERY>
2018-12-10 12:44:27 -0800

    add ILanguage interface

---
+++ Ling/Models/Interfaces/ILanguage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ling.Models.Interfaces
+{
+    public interface ILanguage
+    {
+        //Create
+        Task AddLanguage(Language language);
+
+        //Read
+        Task<List<Language>> GetLanguages();
+        Task<Language> GetLanguage(int id);
+
+        //Update
+        Task UpdateLanguage(Language language);
+
+        //Delete
+        Task DeleteLanguage(int id);
+    }
+}
