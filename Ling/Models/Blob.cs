0afa03f03d7561795261f2b63119852c735bcc19
Kendragon <Kendragon@PHRONTISTERY>
2018-12-11 11:46:06 -0800

    add Blob class properties

--- Ling/Models/Blob.cs
+++ Ling/Models/Blob.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,12 @@ namespace Ling.Models
 {
     public class Blob
     {
+        public CloudStorageAccount CloudStorageAccount { get; set; }
+        public CloudBlobClient CloudBlobClient { get; set; }
+
+        public Blob(string storageAccountName, )
+        {
+
+        }
     }
 }
