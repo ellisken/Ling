615a5132ce6765d2060df337866448a1ed41c03f
Rebecca Hong <rebeccayhong@gmail.com>
2018-12-10 17:12:41 -0800

    Add variables and cite tutorial

--- Ling/wwwroot/js/site.js
+++ Ling/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/* Tutorial to get started with Recorder.js
+ * https://addpipe.com/blog/using-recorder-js-to-capture-wav-audio-in-your-html5-web-site/
+*/
 
-// Write your JavaScript code.
+/* Variables */
+
+// Stream from getUserMedia()
+let getUserMediaStream;
+// Recorder.js object
+let recording;
+// MediaStreamAudioSourceNode we will be recording
+let input;
+
+// Shim for AudioContext when it's not audio video bridging. Shims allow for backwards compatibility by intercepting API calls and providing a layer of abstraction between a caller and a target.
+let AudioContext = window.AudioContext || webkit.AudioContext;
+// New audio context that helps us record
+let audioContext = new AudioContext;
