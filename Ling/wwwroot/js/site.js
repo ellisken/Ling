138c3ef54b8c16f338961a2744821e60bc215646
Rebecca Hong <rebeccayhong@gmail.com>
2018-12-10 22:34:01 -0800

    Tweak methods to account for one button UI

--- Ling/wwwroot/js/site.js
+++ Ling/wwwroot/js/site.js
@@ -22,22 +22,22 @@ const pauseButton = document.getElementById("pause-button");
 const stopButton = document.getElementById("stop-button");
 
 // Attach event listeners
-recordButton.addEventListener("click", startRecording);
-pauseButton.addEventListener("click", pauseRecording);
+recordButton.addEventListener("click", recordButtonHandler);
 stopButton.addEventListener("click", stopRecording);
 
 
 /* Event handlers */
 
-// "Start" recording an audio clip
+// Pause, Resume, or Record based on classes on recordButton
+function recordButtonHandler() {
+    if ($(recordButton).hasClass("inProgress")) {
+        pauseOrResumeRecording();
+    } else {
+        startRecording();
+    }
+}
+
 function startRecording() {
-    console.log("start recording has been clicked!");
-
-    // Disable the record button until getUserMedia() returns a success or fail
-    recordButton.disabled = true;
-    stopButton.disabled = false;
-    pauseButton.disables = false;
-
     /*
       getUserMedia() is a promise-based method that prompts the user for permission to use a media input, which produces a MediaStream object with a specified list of a/v tracks. In our case, the stream wil have an audio track.
         https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
@@ -47,7 +47,11 @@ function startRecording() {
     const constraints = { audio: true, video: false };
 
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-        console.log("GUM success! i'm in the promise's callback!");
+        $(recordButton).addClass("inProgress");
+        toggleEnabledOnRecordButton();
+        stopButton.disabled = false;
+
+        console.log("permissions granted!! i'm in the promise!");
 
         // Assign our getUserMediaStream global variable to the recorded stream for later use
         getUserMediaStream = stream;
@@ -60,29 +64,42 @@ function startRecording() {
 
         // Begin recording
         rec.record();
+        // Set "Record" button text to "Pause"
+        recordButton.innerHTML = "Pause";
 
         console.log("recording has started");
     }).catch((err) => {
         // Enable the "Record" button again if the recording fails
-        recordButton.disabled = false;
-        pauseButton.disabled = true;
         stopButton.disabled = true;
     });
 }
 
-// "Pause" a recording
-function pauseRecording() {
-    console.log("pause hit on rec.recording =", rec.recording);
+function toggleEnabledOnRecordButton() {
+    if ($(recordButton).hasClass("enabled")) {
+        $(recordButton).removeClass("enabled");
+    } else {
+        $(recordButton).addClass("enabled");
+    }
+}
+
+// "Pause" or "Resume" a recording
+function pauseOrResumeRecording() {
+    // Make sure user can stop the recording at any time
+    stopButton.disabled = false;
 
     if (rec.recording) {
+        console.log("pause hit on rec.recording =", rec.recording);
         // Pause
         rec.stop();
         // Change UI text of "Pause" button to "Resume"
-        pauseButton.innerHTML = "Resume";
+        recordButton.innerHTML = "Resume";
+        toggleEnabledOnRecordButton();
     } else {
+        console.log("resume hit on rec.recording =", rec.recording);
         // Resume
         rec.record();
-        pauseButton.innerHTML = "Pause";
+        recordButton.innerHTML = "Pause";
+        toggleEnabledOnRecordButton();
     }
 }
 
@@ -91,12 +108,13 @@ function stopRecording() {
     console.log("Stop button clicked");
 
     // Disable the stop button and enable start/pause to allow new recordings
-    recordButton.disabled = false;
-    pauseButton.disabled = false;
     stopButton.disabled = true;
 
-    // Reset "Pause" button text in case recording is stopped while paused
-    pauseButton.innerHTML = "Pause"
+    // Reset "Record" button text when recording is stopped
+    recordButton.innerHTML = "Record";
+
+    // Remove "inProgress" class to show recording is stopped
+    $(recordButton).removeClass("inProgress");
 
     // Get Recorder object to stop recording
     rec.stop();
@@ -105,7 +123,31 @@ function stopRecording() {
     getUserMediaStream.getAudioTracks()[0].stop();
 
     // Create WAV blob and pass on to uploadToServer
-    rec.exportWAV(uploadToServer);
+    rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {
+
+    var url = URL.createObjectURL(blob);
+    var au = document.createElement('audio');
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+
+    //add controls to the <audio> element
+    au.controls = true;
+    au.src = url;
+
+    //link the a element to the blob
+    link.href = url;
+    link.download = new Date().toISOString() + '.wav';
+    link.innerHTML = link.download;
+
+    //add the new audio and a elements to the li element
+    li.appendChild(au);
+    li.appendChild(link);
+
+    //add the li element to the ordered list
+    recordingsList.appendChild(li);
 }
 
 // Make an AJAX POST request to create a new Recording object in the database
